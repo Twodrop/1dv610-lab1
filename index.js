80cc39e95ebb3bd2abd284e5ce6ac10b0ac2b579
Robin Helander <rehswe@gmail.com>
2026-09-06 13:09:34 +0200

    Add print2DArray function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world! testing")
+
+const alphabet = require('./alphabet')
+
+function print2DArray (array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let y = 0; y < array[i].length; y++) {
+            const element = array[i][y];
+            process.stdout.write(element == 1 ? "██" : "  ");
+        }
+        process.stdout.write("\n")
+
+    }
+}
+
+print2DArray(alphabet["a"])
